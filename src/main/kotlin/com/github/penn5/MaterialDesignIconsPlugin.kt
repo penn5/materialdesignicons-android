29fccc555ff68149fd2a5be7744e81f1a8e5d8e9
Hackintosh 5 <git@hack5.dev>
2023-01-29 18:11:41 +0000

    Switch icon fetching to use mdi CDN

--- src/main/kotlin/com/github/penn5/MaterialDesignIconsPlugin.kt
+++ src/main/kotlin/com/github/penn5/MaterialDesignIconsPlugin.kt
@@ -2,7 +2,6 @@ package com.github.penn5
 
 import com.android.build.gradle.BaseExtension
 import groovy.json.JsonSlurper
-import org.apache.commons.io.input.UnixLineEndingInputStream
 import org.apache.commons.io.output.WriterOutputStream
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
@@ -10,6 +9,8 @@ import org.gradle.api.Project
 import org.gradle.api.tasks.*
 import org.gradle.kotlin.dsl.invoke
 import org.gradle.kotlin.dsl.register
+import org.xml.sax.Attributes
+import org.xml.sax.helpers.DefaultHandler
 import java.io.File
 import java.io.IOException
 import java.io.InputStream
@@ -17,9 +18,9 @@ import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.StandardCharsets
 import java.util.*
+import javax.xml.parsers.SAXParserFactory
 
-private const val META_URL = "https://raw.githubusercontent.com/Templarian/MaterialDesign/master/meta.json"
-private const val BASE_ICON_URL = "https://materialdesignicons.com/api/download/icon/vectordrawable/"
+private const val BASE_CDN_URL = "https://cdn.jsdelivr.net/npm/@mdi/svg"
 
 private const val SIGNATURE = "<!-- File auto-synced, do not edit! MaterialDesignIcons ID: "
 
@@ -37,7 +38,7 @@ class MaterialDesignIconsPlugin : Plugin<Project> {
 }
 
 private fun getMeta(): List<*> {
-    val conn = (URL(META_URL).openConnection() as HttpURLConnection).apply {
+    val conn = (URL("$BASE_CDN_URL/meta.json").openConnection() as HttpURLConnection).apply {
         requestMethod = "GET"
     }
     conn.connect()
@@ -45,8 +46,8 @@ private fun getMeta(): List<*> {
     return json as List<*>
 }
 
-private fun getIcon(iconID: String): InputStream {
-    val conn = (URL(BASE_ICON_URL + iconID).openConnection() as HttpURLConnection).apply {
+private fun getIconSVG(iconName: String): InputStream {
+    val conn = (URL("$BASE_CDN_URL/svg/$iconName.svg").openConnection() as HttpURLConnection).apply {
         requestMethod = "GET"
     }
     conn.connect()
@@ -54,7 +55,57 @@ private fun getIcon(iconID: String): InputStream {
     return conn.inputStream!!
 }
 
+class SVGParser : DefaultHandler() {
+    var path: String? = null
+        private set
+    var viewbox: String? = null
+        private set
+
+    override fun startElement(
+        uri: String,
+        localName: String,
+        qName: String,
+        attributes: Attributes
+    ) {
+        when (qName) {
+            "svg" -> {
+                viewbox = attributes.getValue("viewBox")
+            }
+            "path" -> {
+                path = attributes.getValue("d")
+            }
+            else -> error("Unknown element $qName")
+        }
+        super.startElement(uri, localName, qName, attributes)
+    }
+
+    override fun endElement(uri: String, localName: String, qName: String) {
+        super.endElement(uri, localName, qName)
+    }
+}
+
+private fun getIcon(iconName: String): String {
+    val handler = SVGParser()
+    SAXParserFactory.newInstance().newSAXParser().parse(getIconSVG(iconName), handler)
+    require(handler.viewbox == "0 0 24 24") { "Incorrect viewbox ${handler.viewbox}" }
+    return """
+        <!-- drawable/${sanitizeComment(iconName.replace('-', '_'))}.xml -->
+        <vector xmlns:android="http://schemas.android.com/apk/res/android"
+            android:height="24dp"
+            android:width="24dp"
+            android:viewportWidth="24"
+            android:viewportHeight="24">
+            <path android:fillColor="#000" android:pathData="${escapeAttribute(handler.path!!)}" />
+        </vector>
+        
+    """.trimIndent()
+}
+
 open class UpdateDrawablesTask : DefaultTask() {
+    init {
+        outputs.upToDateWhen { false }
+    }
+
     @TaskAction
     fun doAction() {
         try {
@@ -72,10 +123,11 @@ open class UpdateDrawablesTask : DefaultTask() {
                 }
                 iconID ?: continue
                 val iconMeta = meta[iconID] as Map<*, *>
-                val icon = getIcon(iconID)
+                val icon = getIcon(iconMeta["name"] as String)
                 file.bufferedWriter().use { writer ->
                     writer.write("$SIGNATURE${sanitizeComment(iconID)}-->\n")
-                    if (iconMeta["deprecated"] as Boolean) {
+                    println(iconMeta)
+                    if (iconMeta["deprecated"] as Boolean? == true) {
                         logger.warn("Deprecated icon ${file.path}")
                     } else {
                         // All icons are Apache 2.0 except brand icons, but they're deprecated.
@@ -94,18 +146,13 @@ open class UpdateDrawablesTask : DefaultTask() {
                         writer.write("limitations under the License. -->\n")
                     }
                     WriterOutputStream(writer, StandardCharsets.UTF_8).use { outputStream ->
-                        UnixLineEndingInputStream(icon, true).transferTo(outputStream)
+                        outputStream.write(icon.encodeToByteArray())
                     }
                 }
             }
         } catch (e: IOException) {
             e.printStackTrace()
         }
-    }
-
-    private fun sanitizeComment(text: String): String {
-        require(!text.contains("--")) { "$text must not contain \"--\"" }
-        return text
     }
 
     @Internal
@@ -137,4 +184,15 @@ open class UpdateDrawablesTask : DefaultTask() {
 
     @get:OutputFiles
     val outputFiles get() = files
+}
+
+private fun sanitizeComment(text: String): String {
+    require(!text.contains("--")) { "$text must not contain \"--\"" }
+    return text
+}
+
+private fun escapeAttribute(text: String): String {
+    return text
+        .replace("&", "&amp;")
+        .replace("\"", "&quot;")
 }
